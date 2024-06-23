b9045a2dd31172af7197b8d67beda103b2bfd4da
alfaalexx <alfaalex02@gmail.com>
2024-06-23 20:58:22 +0700

    membuat fitur firebase login dan forgot password

--- lib/pages/auth/loginPage.dart
+++ lib/pages/auth/loginPage.dart
@@ -3,6 +3,7 @@ import 'package:coinwise/pages/dashboard/dashboard_screen.dart';
 import 'package:coinwise/pages/forgot_password/forgot_password_page.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -12,7 +13,148 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginState extends State<LoginPage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passController = TextEditingController();
+
+  String _email = "";
+  String _password = "";
   bool _obscureText = true;
+
+  void _handleLogin() async {
+    if (_email.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Email is required'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    } else if (_password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Password is required'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    } else {
+      try {
+        UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+          email: _email,
+          password: _password,
+        );
+
+        if (userCredential.user != null) {
+          // Cek apakah email pengguna sudah diverifikasi.
+          if (userCredential.user!.emailVerified) {
+            Navigator.pushAndRemoveUntil(context,
+                MaterialPageRoute(builder: (context) {
+              return const DashboardScreen();
+            }), (route) => false);
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Login Successfully'),
+                backgroundColor: Colors.green,
+              ),
+            );
+          } else {
+            // Pengguna belum memverifikasi alamat email mereka.
+            // Tampilkan pesan kesalahan atau tindakan yang sesuai.
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Email Verification Required"),
+                  content: Text("Please verify your email before logging in."),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text("Resend Email Verification"), // Tombol baru
+                      onPressed: () async {
+                        if (userCredential.user != null &&
+                            !userCredential.user!.emailVerified) {
+                          try {
+                            await userCredential.user!.sendEmailVerification();
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text('Email verification sent'),
+                                backgroundColor: Colors.green,
+                              ),
+                            );
+                          } catch (e) {
+                            print("Error sending verification email: $e");
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content:
+                                    Text('Error resending verification email'),
+                                backgroundColor: Colors.red,
+                              ),
+                            );
+                          }
+                        } else if (userCredential.user != null &&
+                            userCredential.user!.emailVerified) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Email is already verified'),
+                              backgroundColor: Colors.green,
+                            ),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('No user currently signed in'),
+                              backgroundColor: Colors.red,
+                            ),
+                          );
+                        }
+                        Navigator.pop(
+                            context); // Tutup dialog setelah tombol ditekan
+                      },
+                    ),
+                    TextButton(
+                      child: Text("OK"),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+
+            // Logout pengguna karena mereka belum memverifikasi email mereka.
+            await _auth.signOut();
+          }
+        }
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found') {
+          print("User not found for email: $_email");
+          // Tindakan untuk kesalahan "user not found"
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text("No user found for that email."),
+              backgroundColor: Colors.red,
+            ),
+          );
+        } else if (e.code == 'wrong-password') {
+          // Tindakan untuk kesalahan "wrong password"
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Wrong password provided for that user."),
+              backgroundColor: Colors.red,
+            ),
+          );
+        }
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("Error During Login: $e"),
+            backgroundColor: Colors.red,
+          ),
+        );
+        // print("Error During Login: $e");
+      }
+    }
+  }
 
   void _toggle() {
     setState(() {
@@ -35,100 +177,128 @@ class _LoginState extends State<LoginPage> {
             Padding(
               padding:
                   const EdgeInsets.symmetric(horizontal: 25.0, vertical: 30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // INPUT EMAIL
-                  const Text(
-                    'Email',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 3.0),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'user123@gmail.com',
-                      prefixIcon: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Image.asset(
-                          'assets/images/email_icon.png',
-                          width: 24,
-                          height: 24,
-                        ),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // INPUT EMAIL
+                    const Text(
+                      'Email',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
                       ),
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 16.0, vertical: 10.0),
                     ),
-                  ),
-                  const SizedBox(height: 10.0),
-
-                  // INPUT SANDI
-                  const Text(
-                    'Kata sandi',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 3.0),
-                  TextField(
-                    obscureText: _obscureText,
-                    decoration: InputDecoration(
-                      hintText: 'Kata Sandi',
-                      prefixIcon: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Image.asset(
-                          'assets/images/password.png',
-                          width: 24,
-                          height: 24,
-                        ),
-                      ),
-                      suffixIcon: IconButton(
-                          icon: Icon(_obscureText
-                              ? Icons.visibility_off
-                              : Icons.visibility),
-                          onPressed: _toggle),
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 16.0, vertical: 10.0),
-                    ),
-                  ),
-                  const SizedBox(height: 10.0),
-                  Container(
-                    alignment: Alignment.bottomRight,
-                    child: InkWell(
-                      onTap: () {
-                        //Navigator ke Forgot Password Page
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ForgotPasswordPage(),
+                    const SizedBox(height: 3.0),
+                    TextFormField(
+                      controller: _emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        hintText: 'user123@gmail.com',
+                        prefixIcon: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Image.asset(
+                            'assets/images/email_icon.png',
+                            width: 24,
+                            height: 24,
                           ),
-                        );
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 10.0),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Masukkan Email Anda";
+                        }
+                        return null;
                       },
-                      child: Text(
-                        "Lupa Kata Sandi?",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            color: Colors.lightBlue,
-                            decoration: TextDecoration.underline),
+                      onChanged: (value) {
+                        setState(() {
+                          _email = value;
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 10.0),
+
+                    // INPUT SANDI
+                    const Text(
+                      'Kata sandi',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  )
-                ],
+                    const SizedBox(height: 3.0),
+                    TextFormField(
+                      controller: _passController,
+                      obscureText: _obscureText,
+                      decoration: InputDecoration(
+                        hintText: 'Kata Sandi',
+                        prefixIcon: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Image.asset(
+                            'assets/images/password.png',
+                            width: 24,
+                            height: 24,
+                          ),
+                        ),
+                        suffixIcon: IconButton(
+                            icon: Icon(_obscureText
+                                ? Icons.visibility_off
+                                : Icons.visibility),
+                            onPressed: _toggle),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 10.0),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Masukkan Kata Sandi Anda";
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        setState(() {
+                          _password = value;
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 10.0),
+                    Container(
+                      alignment: Alignment.bottomRight,
+                      child: InkWell(
+                        onTap: () {
+                          //Navigator ke Forgot Password Page
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ForgotPasswordPage(),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          "Lupa Kata Sandi?",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: Colors.lightBlue,
+                              decoration: TextDecoration.underline),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
 
@@ -144,13 +314,9 @@ class _LoginState extends State<LoginPage> {
                   ),
                 ),
                 onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                          const DashboardScreen(),
-                    ),
-                  );
+                  if (_formKey.currentState!.validate()) {
+                    _handleLogin();
+                  }
                 },
                 child: const Text(
                   'Masuk',
--- lib/pages/forgot_password/forgot_password_page.dart
+++ lib/pages/forgot_password/forgot_password_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Import Firebase Core
 
 class ForgotPasswordPage extends StatefulWidget {
   const ForgotPasswordPage({super.key});
@@ -8,6 +9,49 @@ class ForgotPasswordPage extends StatefulWidget {
 }
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final TextEditingController _emailController = TextEditingController();
+
+  Future<void> _resetPassword() async {
+    if (_emailController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Email is required'),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return;
+    }
+
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(
+        email: _emailController.text,
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Password reset email sent. Check your email.'),
+          backgroundColor: Colors.green,
+        ),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('The email is not registered. Please sign up.'),
+            backgroundColor: Colors.red,
+          ),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Error: $e'),
+            backgroundColor: Colors.red,
+          ),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,97 +69,107 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.only(top: 50),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    alignment: Alignment.topCenter,
-                    child:
-                        Image.asset("assets/images/forgot_password_image.png"),
+          child: Form(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      alignment: Alignment.topCenter,
+                      child: Image.asset(
+                          "assets/images/forgot_password_image.png"),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
                   ),
-                ],
-              ),
-              const SizedBox(height: 30),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
+                  height: 1000,
+                  child: Padding(
+                    padding: const EdgeInsets.all(30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Pemulihan Kata Sandi",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          "Link pemulihan kata sandi akan dikirim melalui email anda.",
+                          style: TextStyle(
+                            fontSize: 14,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        TextFormField(
+                          controller: _emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            hintText: 'Masukkan Alamat Email Anda',
+                            prefixIcon: Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Image.asset(
+                                'assets/images/email_icon.png',
+                                width: 24,
+                                height: 24,
+                              ),
+                            ),
+                            filled: true,
+                            fillColor: Color(0xffE5EBF3),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 16.0, vertical: 10.0),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Masukkan Alamat Email Anda";
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(height: 20),
+                        SizedBox(
+                          width: double.infinity,
+                          child: TextButton(
+                            style: TextButton.styleFrom(
+                              backgroundColor: const Color(0xFF023E8A),
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 12.0, horizontal: 120.0),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0),
+                              ),
+                            ),
+                            onPressed: _resetPassword,
+                            child: const Text(
+                              'Kirim',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                height: 1000,
-                child: Padding(
-                  padding: const EdgeInsets.all(30),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Pemulihan Kata Sandi",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        "Link pemulihan kata sandi akan dikirim melalui email anda.",
-                        style: TextStyle(
-                          fontSize: 14,
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Masukkan Alamat Email Anda',
-                          prefixIcon: Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Image.asset(
-                              'assets/images/email_icon.png',
-                              width: 24,
-                              height: 24,
-                            ),
-                          ),
-                          filled: true,
-                          fillColor: Color(0xffE5EBF3),
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide.none,
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          contentPadding: const EdgeInsets.symmetric(
-                              horizontal: 16.0, vertical: 10.0),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      SizedBox(
-                        width: double.infinity,
-                        child: TextButton(
-                          style: TextButton.styleFrom(
-                            backgroundColor: const Color(0xFF023E8A),
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 12.0, horizontal: 120.0),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0),
-                            ),
-                          ),
-                          onPressed: () {},
-                          child: const Text(
-                            'Kirim',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
